09cb2d686ed1a0f2a004a708f9eed97901419fcb
Khang <khangdogia2002@gmail.com>
2024-05-24 16:56:25 +0700

    Add method addUser in Controller

--- AccountService/src/main/java/com/example/accountservice/controller/UserController.java
+++ AccountService/src/main/java/com/example/accountservice/controller/UserController.java
@@ -1,9 +1,25 @@
 package com.example.accountservice.controller;
 
+import com.example.accountservice.dto.request.CreateUserRequest;
+import com.example.accountservice.dto.response.ApiResponse;
+import com.example.accountservice.dto.response.UserResponse;
+import com.example.accountservice.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ApiResponse<UserResponse> addUser(@RequestBody CreateUserRequest request){
+        return ApiResponse.<UserResponse>builder()
+                .data(userService.create(request))
+                .build();
+    }
 }
--- AccountService/src/main/java/com/example/accountservice/service/UserService.java
+++ AccountService/src/main/java/com/example/accountservice/service/UserService.java
@@ -1,7 +1,32 @@
 package com.example.accountservice.service;
 
+import com.example.accountservice.dto.request.CreateUserRequest;
+import com.example.accountservice.dto.response.UserResponse;
+import com.example.accountservice.mapper.UserMapper;
+import com.example.accountservice.model.User;
+import com.example.accountservice.repo.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    public UserResponse create(CreateUserRequest request){
+        User user = userMapper.toUser(request);
+
+        user.setLock(true);
+        user.setActivate(false);
+        return userMapper.toUserResponse(userRepository.save(user));
+    }
+
+    public List<UserResponse> getUsers(){
+        return (userRepository.findAll().stream().map(userMapper::toUserResponse).toList());
+    }
 }
