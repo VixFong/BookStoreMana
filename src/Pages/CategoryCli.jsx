23a509a99716842d10168b1eea44775106889c70
VPhong <viphong123456789@gmail.com>
2024-06-22 01:53:53 +0700

    Added Shop Component (Category Client)

---
+++ src/Pages/CategoryCli.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Heading from '../Component/Heading'
+import Shop from '../Component/CategoryClient/Shop'
+
+export const CategoryCli = () => {
+  return (
+    <div>
+        <Heading/>
+        <Shop/>
+    </div>
+  )
+}
